871abe96e531bd45974f7585a90a68367869cbc9
kubakarminski <jlkarminski@gmail.com>
2017-12-20 18:50:49 +0100

    Deprimo codare

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
+var express  = require('express');
+var app      = express();
+var router   = express.Router();
+const config = {
+	PORT: 3000,
+}
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.sendFile('index.html');
+router.get('/', function (req, res) {
+	res.sendFile('index.html');
 });
 
-router.post('/', function(req, res) {
-    console.log('post halko');
-    res.type('text');
-    res.write('halko post');
-    res.end();
+router.post('/', function (req, res) {
+	console.log('post halko');
+	res.type('text');
+	res.write('halko post');
+	res.end();
 });
 app.use(express.static(__dirname + '/server/public/'));
 app.use(router);
-app.listen(3000);
+app.listen(config.PORT);
+console.log(`Starting web server on port 3000 ${config.PORT}`);
 
 module.exports = router;
