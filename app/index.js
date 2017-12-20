564769e349b00fb94c807366550477aef6948ad7
Przemek Ferkaluk <pferkaluk@gmail.com>
2017-12-20 16:23:49 +0100

    create poc

--- app/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('No witam ciepło'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
